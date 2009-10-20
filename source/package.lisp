1cb1364d34e6099d3bfaea67c168c4b9468bb44a
levente.meszaros <levente.meszaros@gmail.com>
2009-10-20 14:26:05 +0000

    Rneame to export-external-*

--- source/package.lisp
+++ source/package.lisp
@@ -9,17 +9,19 @@
 (defpackage :hu.dwim.common-lisp)
 
 (defpackage :hu.dwim.common
-  (:use :hu.dwim.common-lisp
-        :alexandria
+  (:use :alexandria
         :anaphora
         :closer-mop
+        :hu.dwim.common-lisp
         :iterate
         :metabang-bind)
-  (:export #:reexport-exported-symbols
-           #:reexport-exported-symbols-of-used-packages))
 
-;; we can not (in-package :hu.dwim.common) yet, because we are just setting it up now.
-;; so, we do all this in :cl-user and intern a few symbols there needed for the implementation below.
+  (:export #:export-external-symbols
+           #:export-external-symbols-of-used-packages))
+
+;;;;;;
+;;; We can not (in-package :hu.dwim.common) yet, because we are just setting it up now.
+;;; So, we do all this in :common-lisp-user and intern a few symbols there needed for the implementation below.
 
 (defparameter hu.dwim.common::*common-lisp-symbols-absent-in-hu.dwim.common-lisp*
   '(common-lisp:time
@@ -27,7 +29,7 @@
     common-lisp:set
     ))
 
-(defun hu.dwim.common::reexport-exported-symbols (source-package target-package &key filter)
+(defun hu.dwim.common::export-external-symbols (source-package target-package &key filter)
   (setf target-package (find-package target-package))
   (unless filter
     (setf filter (constantly t)))
@@ -38,13 +40,13 @@
         (import symbol target-package)
         (export symbol target-package)))))
 
-(defun hu.dwim.common::reexport-exported-symbols-of-used-packages (package &key filter)
+(defun hu.dwim.common::export-external-symbols-of-used-packages (package &key filter)
   (dolist (used-package (package-use-list (find-package package)))
-    (hu.dwim.common::reexport-exported-symbols used-package package :filter filter)))
+    (hu.dwim.common::export-external-symbols used-package package :filter filter)))
 
-(hu.dwim.common::reexport-exported-symbols
+(hu.dwim.common::export-external-symbols
  :common-lisp :hu.dwim.common-lisp
  :filter (lambda (symbol)
            (not (member symbol hu.dwim.common::*common-lisp-symbols-absent-in-hu.dwim.common-lisp*))))
 
-(hu.dwim.common::export-exported-symbols-of-used-packages :hu.dwim.common)
+(hu.dwim.common::export-external-symbols-of-used-packages :hu.dwim.common)
