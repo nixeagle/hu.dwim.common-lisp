4172a16e929fa71dba7d3787a016ebe613fb947c
attila.lendvai <attila.lendvai@gmail.com>
2009-10-20 13:42:46 +0000

    split to :hu.dwim.common-lisp and :hu.dwim.common (WARNING: breaks many other hu.dwim.* projects as of writing this note)

--- source/package.lisp
+++ source/package.lisp
@@ -6,17 +6,48 @@
 
 (in-package :common-lisp-user)
 
-(defpackage :hu.dwim.common-lisp
-  (:use :alexandria
+(defpackage :hu.dwim.common-lisp)
+
+(defpackage :hu.dwim.common
+  (:use :hu.dwim.common-lisp
+        :alexandria
         :anaphora
         :closer-mop
-        :common-lisp
         :iterate
-        :metabang-bind))
+        :metabang-bind)
+  (:export #:reexport-exported-symbols
+           #:reexport-exported-symbols-of-used-packages))
 
-(in-package :hu.dwim.common-lisp)
+;; we can not (in-package :hu.dwim.common) yet, because we are just setting it up now.
+;; so, we do all this in :cl-user and intern a few symbols there needed for the implementation below.
 
-(dolist (package (package-use-list (find-package :hu.dwim.common-lisp)))
-  (do-external-symbols (symbol package)
-    (unless (member symbol '(common-lisp:time common-lisp:set))
-      (export (list symbol)))))
+(defparameter hu.dwim.common::*common-lisp-symbols-absent-in-hu.dwim.common-lisp*
+  '(common-lisp:time
+    ;; common-lisp:get computed-class uses it for now
+    common-lisp:set
+    ))
+
+(defun hu.dwim.common::reexport-exported-symbols (source-package target-package &key filter)
+  (setf target-package (find-package target-package))
+  (unless filter
+    (setf filter (constantly t)))
+  (do-external-symbols (symbol source-package)
+    (when (funcall filter symbol)
+      ;; do take care of the symbol nil: (list nil)!
+      (let ((symbol (or symbol (list nil))))
+        (import symbol target-package)
+        (export symbol target-package)))))
+
+(defun hu.dwim.common::reexport-exported-symbols-of-used-packages (package &key filter)
+  (dolist (used-package (package-use-list (find-package package)))
+    (hu.dwim.common::reexport-exported-symbols used-package package :filter filter)))
+
+(hu.dwim.common::reexport-exported-symbols
+ :common-lisp :hu.dwim.common-lisp
+ :filter (lambda (symbol)
+           (not (member symbol hu.dwim.common::*common-lisp-symbols-absent-in-hu.dwim.common-lisp*))))
+
+(hu.dwim.common::reexport-exported-symbols-of-used-packages
+ :hu.dwim.common
+ :filter (lambda (symbol)
+           (not (member symbol hu.dwim.common::*common-lisp-symbols-absent-in-hu.dwim.common-lisp*))))
